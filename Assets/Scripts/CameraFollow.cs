4e52474d7d780cf8c6228500abf4d8d4a9806718
MustachedNinja <kostya.yatsuk9@live.com>
2021-03-06 02:15:50 -0800

    - Add camera follow and light controller

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] private bool zoom = true;
+    [SerializeField] private float minZoomLimit = 5f;
+    [SerializeField] private Camera orthoCamera;
+
+    void LateUpdate()
+    {
+        float maxOffCenter = 0;
+        if (zoom) {
+            maxOffCenter = Mathf.Max(Mathf.Abs(player1.position.y), Mathf.Abs(player2.position.y));
+            orthoCamera.orthographicSize = Mathf.Max(maxOffCenter, minZoomLimit);
+        }
+        Vector3 center = new Vector3((player1.position.x + player2.position.x) / 2, 0, 0);
+        Vector3 targetPosition = center + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.fixedDeltaTime);
+        transform.position = smoothedPosition;
+        
+    }
+}
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private float minIntensityDistance = 20f;
+    [SerializeField] private float maxIntensityDistance = 5f;
+    [SerializeField] private float maxIntensity = 1f;
+
+    private Light2D[] lights;
+
+    void Start() {
+        lights = transform.GetComponentsInChildren<Light2D>(true);
+    }
+
+    void SetIntensity(float intensity) {
+        foreach(Light2D l in lights) {
+            l.intensity = intensity;
+        }
+    }
+
+    float CalculateIntensity(Vector2 player1Pos, Vector2 player2Pos) {
+        float distance = Vector2.Distance(player1Pos, player2Pos);
+        if (distance > minIntensityDistance) {
+            return 0;
+        } else if (distance < maxIntensityDistance) {
+            return maxIntensity;
+        } else {
+            return Mathf.InverseLerp(minIntensityDistance, maxIntensityDistance, distance) * maxIntensity;
+        }
+    }
+
+    void Update() {
+        SetIntensity(CalculateIntensity(player1.position, player2.position));
+    }
+
+}
